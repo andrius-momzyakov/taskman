94f187f4022ba9bc479e525938411789fd0fc08f
andrius_momzyakov <Andrius_1972@>
2016-06-06 12:02:12 +0300

    Db view fixed.

--- taskman/task/SQL/task_vtask.sql
+++ taskman/task/SQL/task_vtask.sql
@@ -1,5 +1,30 @@
-create or replace view task_vtask as
+create or replace view task_vtask_null as
 select t.*, tup.prty
 from task_task t left outer join
-(select max(p.id) prty, p.user_id, p.task_id from task_taskuserpriority p
-  group by p.user_id, p.task_id) as tup on t.id = tup.task_id;
+( select t.prty, t.user_id, t.task_id
+  from
+    (select max(p.id) prty, p.user_id user_id, p.task_id task_id from task_taskuserpriority p
+     group by p.user_id, p.task_id) as t
+) as tup on t.id = tup.task_id;
+
+create or replace view task_vtask as
+select
+    t.id,
+    t.subject,
+    t."desc",
+    t.deadline_date,
+    t.notify_before,
+    t.created,
+    t.updated,
+    t.closed,
+    t.close_reason,
+    t.created_by_id,
+    t.parent_id,
+    t.updated_by_id,
+    t.executor_id,
+    t.type_id,
+    t.module_id,
+    t.project_id,
+    t.status,
+    coalesce(t.prty, -1) prty
+from task_vtask_null t;
