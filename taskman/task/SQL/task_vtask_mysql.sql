ce6a976cd167749febb1849944d02a401a813fff
Andrew Momzyakoff <andrius.momzyakov@gmail.com>
2016-06-11 20:50:05 +0300

    Priority field's default value set yo zero.

--- taskman/task/SQL/task_vtask_mysql.sql
+++ taskman/task/SQL/task_vtask_mysql.sql
@@ -3,6 +3,6 @@ from task_taskuserpriority p
 group by p.user_id, p.task_id;
 
 create or replace view task_vtask as
-select t.*, tup.prty
+select t.*, IFNULL(tup.prty, 0) prty
 from task_task t left outer join
 task_vtmp as tup on t.id = tup.task_id;
